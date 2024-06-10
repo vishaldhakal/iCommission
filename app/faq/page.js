bb095e4b455ffd4d568bda4064b4cfdc4280c684
vishaldhakal <vishaldhakal96@gmail.com>
2024-06-10 09:14:55 +0545

    design updated

--- app/faq/page.js
+++ app/faq/page.js
@@ -1,6 +1,12 @@
 import ContactUs from "@/components/ContactUs";
 import FAQ from "@/components/FAQ";
 
+export const metadata = {
+  title: "iCommission Advance - Frequently Asked Questions",
+  description:
+    "Any questions about iCommission Advance? Check out our FAQ page to find answers to the most common questions!",
+};
+
 function FAQPage() {
   return (
     <>
