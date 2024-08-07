9b8c727f52e03d8df938c946353bb34aee083558
vishaldhakal <vishaldhakal96@gmail.com>
2024-08-07 16:39:15 +0545

    chore: Add NextTopLoader component to RootLayout

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 /* import "bootstrap/dist/css/bootstrap.min.css"; */
 import "./globals.css";
 import GoogleAnalytics from "./GoogleAnalytics";
+import NextTopLoader from "nextjs-toploader";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <NextTopLoader />
         <GoogleAnalytics />
         {children}
       </body>
